e2aee80081b37eb194718565ec6027648e2c5292
Alfonso Matsuoka <alf.matsuoka2000@gmail.com>
2025-06-19 17:42:18 -0300

    feat: adding .env and dotenv to the project.

--- src/modules/categoria-produto/categoria-produto.module.ts
+++ src/modules/categoria-produto/categoria-produto.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { CategoriaProdutoService } from './categoria-produto.service';
 import { CategoriaProdutoController } from './categoria-produto.controller';
+import { CategoriaProduto } from './entities/categoria-produto.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([CategoriaProduto])],
   controllers: [CategoriaProdutoController],
-  providers: [CategoriaProdutoService]
+  providers: [CategoriaProdutoService],
 })
 export class CategoriaProdutoModule {}
--- src/modules/status-pedido/status-pedido.module.ts
+++ src/modules/status-pedido/status-pedido.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { StatusPedidoService } from './status-pedido.service';
 import { StatusPedidoController } from './status-pedido.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { StatusPedido } from './entities/status-pedido.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([StatusPedido])],
   controllers: [StatusPedidoController],
-  providers: [StatusPedidoService]
+  providers: [StatusPedidoService],
 })
 export class StatusPedidoModule {}
